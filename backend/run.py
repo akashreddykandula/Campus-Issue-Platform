4e78cd7ba5aba58dbf27b5671ee6fcf72fb1f57d
akashreddykandula <akashreddykandula@gmail.com>
2026-07-06 11:35:43 +0530

    added debug  logs in run.py

--- backend/run.py
+++ backend/run.py
@@ -10,6 +10,11 @@ import os
 from app import create_app
 
 app = create_app(os.environ.get("FLASK_ENV", "development"))
+print("FLASK_ENV =", os.environ.get("FLASK_ENV"))
+
+print("SESSION_COOKIE_SAMESITE =", app.config["SESSION_COOKIE_SAMESITE"])
+
+print("SESSION_COOKIE_SECURE =", app.config["SESSION_COOKIE_SECURE"])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
